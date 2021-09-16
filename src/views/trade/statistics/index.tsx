0ee77fb174af6a4ca4eab081446717ec3c677f54
norkts <huangce.zb@alibaba-inc.com>
2021-09-16 08:58:00 +0800

    finish trade page i18n

--- src/views/trade/statistics/index.tsx
+++ src/views/trade/statistics/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
-import { FormattedMessage } from "react-intl";
+import {FormattedMessage, useIntl} from "react-intl";
 
 import { Row, Col, Tabs } from "antd";
 
@@ -13,17 +13,25 @@ function Statistics() {
     console.log(key);
   }
 
+  const {formatMessage} = useIntl()
+
+  function intl(id:string) {
+    return formatMessage({id})
+  }
+
+  const $t = intl
+
   return (
     <Row className="main-block statistics-container">
       <Col flex="100%">
         <Tabs defaultActiveKey="1" onChange={callback}>
-          <TabPane tab={<FormattedMessage id="trade.my.position" />} key="1">
+          <TabPane tab={<FormattedMessage id="Trade.MyPosition.List.MyPosition" />} key="1">
             <MyPosition />
           </TabPane>
-          <TabPane tab={<FormattedMessage id="trade.current.order" />} key="2">
+          <TabPane tab={<FormattedMessage id="Trade.CurrentOrder.List.CurrentOrder" />} key="2">
             <CurrentOrder/>
           </TabPane>
-          <TabPane tab={<FormattedMessage id="trade.trade.history" />} key="3">
+          <TabPane tab={<FormattedMessage id="Trade.TradeHistory.List.TradeHistory" />} key="3">
             {/* <Table dataSource={dataSource} columns={columns} pagination={false}/> */}
           </TabPane>
         </Tabs>
