bb4227a3018bbc6b7e005659bc9080b29c3dd06a
norkts <huangce.zb@alibaba-inc.com>
2021-09-18 00:58:12 +0800

    finish candle lines

--- src/views/trade/dataPanel/chart.tsx
+++ src/views/trade/dataPanel/chart.tsx
@@ -2,7 +2,7 @@ import React, {forwardRef, useEffect, useRef, useState} from 'react';
 
 import CommonCharts from '@/components/charts';
 
-import getEchartsOptions from "@/utils/kline";
+import getEchartsOptions, {buildEchartsOptions} from "@/utils/kline";
 import {ModalProps} from "antd/es/modal";
 import {OpenType, RateType} from "@/views/trade/operation";
 
@@ -22,50 +22,50 @@ function splitData(rawData: any) {
 
 var data0 = splitData([]);
 
-const options1 = {
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross'
-    }
-  },
-  xAxis: {
-    type: 'category',
-    data: data0.categoryData,
-    scale: true,
-    boundaryGap: false,
-    axisLine: { onZero: false },
-    splitLine: { show: false },
-    splitNumber: 20,
-    min: 'dataMin',
-    max: 'dataMax'
-  },
-  yAxis: {
-    // show: false,
-    position: 'right',
-    splitLine:{
-      show: true,
-      lineStyle:{
-        color:'rgba(255,255,255,0.3)'
-      }
-    },
-    scale: true,
-  },
-
-  series: [
-    {
-      name: '1d',
-      type: 'candlestick',
-      data: data0.values,
-      itemStyle: {
-        color: '#E7446B',
-        color0: '#00C49A',
-        borderColor: '#E7446B',
-        borderColor0: '#00C49A'
-      },
-    },
-  ]
-};
+// const options1 = {
+//   tooltip: {
+//     trigger: 'axis',
+//     axisPointer: {
+//       type: 'cross'
+//     }
+//   },
+//   xAxis: {
+//     type: 'category',
+//     data: data0.categoryData,
+//     scale: true,
+//     boundaryGap: false,
+//     axisLine: { onZero: false },
+//     splitLine: { show: false },
+//     splitNumber: 20,
+//     min: 'dataMin',
+//     max: 'dataMax'
+//   },
+//   yAxis: {
+//     // show: false,
+//     position: 'right',
+//     splitLine:{
+//       show: true,
+//       lineStyle:{
+//         color:'rgba(255,255,255,0.3)'
+//       }
+//     },
+//     scale: true,
+//   },
+//
+//   series: [
+//     {
+//       name: '1d',
+//       type: 'candlestick',
+//       data: data0.values,
+//       itemStyle: {
+//         color: '#E7446B',
+//         color0: '#00C49A',
+//         borderColor: '#E7446B',
+//         borderColor0: '#00C49A'
+//       },
+//     },
+//   ]
+// };
 
 interface ChartModalProps extends ModalProps {
   token: string;
@@ -81,6 +81,9 @@ const Chart: React.FC<ChartModalProps> = props => {
 
   const chartRef = useRef<any>()
 
+  const options1 = buildEchartsOptions({categoryData:[(new Date()).Format('hh:mm')]
+    , values:[[0,0,0,0]], curPrice:0
+    , bar:'15m'})
   useEffect(() => {
     const {token,bar,after,before,limit,curPrice} = props
 
@@ -95,7 +98,7 @@ const Chart: React.FC<ChartModalProps> = props => {
       }).catch((e) => {
         console.log(e)
     })
-  } ,[props])
+  } ,[props,chartRef])
 
   return <div className="charts-container">
     <CommonCharts  options={options1} height={380} ref={chartRef}/>
