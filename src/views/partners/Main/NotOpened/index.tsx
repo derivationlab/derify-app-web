3d0b7323a6cabe6567b1a8d94e5119ec3bcccdfd
norkts <huangce.zb@alibaba-inc.com>
2021-09-20 20:53:38 +0800

    finish broker page

--- src/views/partners/Main/NotOpened/index.tsx
+++ src/views/partners/Main/NotOpened/index.tsx
@@ -1,62 +1,129 @@
-import React, { useState } from "react";
-import { Row, Col, Button, Modal, Select } from "antd";
+import React, {useCallback, useEffect, useState} from "react";
+import {Row, Col, Button, Modal, Select, Statistic} from "antd";
+import {useIntl} from "react-intl";
+import {BondAccountType, fromContractUnit, toContractUnit} from "@/utils/contractUtil";
+import {BrokerModel, RootStore} from "@/store";
+import {ModalProps} from "antd/es/modal";
+import {useDispatch, useSelector} from "react-redux";
+import {fck} from "@/utils/utils";
+import ErrorMessage from "@/components/ErrorMessage";
 
 const { Option } = Select;
-interface NotOpenedProps  {
+interface NotOpenedProps extends ModalProps {
   onOK:()=>void
+  onSumitSuccess?: () => void
 }
-const NotOpened:React.FC<NotOpenedProps> = ({onOK})=>{
+const NotOpened:React.FC<NotOpenedProps> = (props)=>{
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const dispatch = useDispatch();
+
+  const {formatMessage} = useIntl();
+  function intl<T>(id:string,values:T[] = []) {
+
+    const intlValues:{[key:number]:T} = {}
+
+    values.forEach((item, index) => {
+      intlValues[index] = item
+    })
+
+
+    return formatMessage({id}, intlValues)
+  }
+
+  const $t = intl;
+
+  const applyBurnAmount = 60000;
+
+  const [accountType, setAccountType] = useState<number>(BondAccountType.DerifyAccount);
+  const [errorMsg, setErrorMsg] = useState<string|React.ReactNode>("");
+
+
+  const {selectedAddress} = useSelector((state:RootStore) => state.user);
+  const {broker,wallet} = useSelector((state:RootStore) => state.broker);
+
+  const getMaxSize = useCallback((wallet, accountType) => {
+    if(accountType === BondAccountType.DerifyAccount) {
+      return fromContractUnit(wallet.derifyEdrfBalance)
+    }else{
+      return fromContractUnit(wallet.walletEdrfBalance)
+    }
+  },[]);
+
+  const onSubmit = useCallback(() => {
+
+    if(!selectedAddress){
+      return;
+    }
+
+    if(applyBurnAmount > getMaxSize(wallet,accountType)){
+      setErrorMsg($t('Broker.Apply.InsufficientAccountBalanceError'));
+      return;
+    }
+
+    setErrorMsg("");
+
+    const applyBrokerAction = BrokerModel.actions.applyBroker({trader:selectedAddress,accountType:accountType, amount: toContractUnit(applyBurnAmount)});
+    //TODO pendding
+    applyBrokerAction(dispatch).then(() => {
+      dispatch(BrokerModel.actions.getTraderBrokerInfo(selectedAddress));
+      setIsModalVisible(false);
+    }).catch(e => {
+      console.error('applyBrokerAction,e',e)
+    });
+
+  },[selectedAddress,accountType,wallet])
+
+  useEffect(() => {
+    if(!selectedAddress || !isModalVisible){
+      return;
+    }
+
+    dispatch(BrokerModel.actions.getBrokerBalance({trader: selectedAddress, accountType: accountType}));
+  }, [isModalVisible,selectedAddress, accountType])
+
   return (
     <Row className="not-opened-container" justify="center">
-      <Col></Col>
-      <Col className="margin-b-m">您尚未开通经纪商身份</Col>
+      <Col className="margin-b-m">{$t("Broker.Apply.NotBrokerMessage")}</Col>
       <Col>
         <Button
           type="primary"
           size="large"
           onClick={() => setIsModalVisible(true)}
         >
-          申请成为经纪商
+          {$t("Broker.Apply.ApplyBroker")}
         </Button>
       </Col>
       <Modal
-        title="开通经纪商"
+        title={$t("Broker.Apply.ApplyBroker")}
         width={300}
         getContainer={false}
         visible={isModalVisible}
-        onOk={() =>{
-          setIsModalVisible(false);
-          onOK()
-        }}
+        onOk={onSubmit}
         onCancel={() => setIsModalVisible(false)}
       >
+        <ErrorMessage style={{margin: "10px 0"}} msg={errorMsg} visible={!!errorMsg} onCancel={() => setErrorMsg("")}/>
         <Row >
           <Col flex="100%" className="margin-b-m">
             <Row className="column-flex">
-              <Col className="margin-b-s">您需要燃烧</Col>
-              <Col className="margin-b-s">
-                <span className="main-color">60,000</span>
-                <span>eDRF</span>
-              </Col >
-              <Col className="margin-b-s">以成为经纪商</Col>
+              <Col className="margin-b-s">{$t("Broker.Apply.GetBrokersPrivilege", [<span className="main-color"><Statistic value={applyBurnAmount}/></span>])}</Col>
             </Row>
           </Col>
           <Col flex="100%" className="margin-b-m">
-            <Select
-              defaultValue="Derify"
-              size="large"
-              style={{ width: "100%" }}
-            >
-              <Option value="Derify">Derify 账户</Option>
-              <Option value="lucy">Lucy</Option>
-              <Option value="Yiminghe">yiminghe</Option>
+            <Select defaultValue={accountType} size="large"
+                    style={{ width: "100%" }}
+                    onChange={(val) => {
+                      setAccountType(val);
+                      setErrorMsg("");
+                    }}
+                    getPopupContainer={trigger => trigger.parentNode}>
+              <Option value={BondAccountType.DerifyAccount}>{$t("Broker.Apply.eDRFAccount")}</Option>
+              <Option value={BondAccountType.WalletAccount}>{$t("Broker.Apply.MyWallet")}</Option>
             </Select>
           </Col>
           <Col flex="100%" className="margin-b-m">
             <Row justify="space-between">
-              <Col>可用余额：</Col>
-              <Col>1234567.00000000 eDRF</Col>
+              <Col>{$t("Broker.Apply.AccountBalance")}：</Col>
+              <Col>{fck(getMaxSize(wallet,accountType), 0,4)} eDRF</Col>
             </Row>
           </Col>
         </Row>
