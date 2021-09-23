a0cb610a063a471ca191f522baf15d324c8fae0b
norkts <huangce.zb@alibaba-inc.com>
2021-09-23 13:35:12 +0800

    fix broker bind redirect

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { RouteProps } from "@/router/types";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { RootStore } from "@/store/index";
+import {useDispatch, useSelector} from "react-redux";
+import {RootStore, UserModel} from "@/store/index";
 import Nav from "./nav";
 import IntlPro from "@/locales/index";
 
@@ -10,33 +10,32 @@ import "./index.less";
 import {bindBroker} from "@/api/broker";
 
 interface HomeProps extends RouteProps {}
-const brokerBindPath = "/partners/bind";
+const brokerBindPath = "/broker/bind";
 
 const Home: React.FC<HomeProps> = props => {
-  const isBind: boolean | undefined = useSelector(
-    (state: RootStore) => state.user.hasBroker
-  );
-
+  const dispatch = useDispatch();
   const { routes, history, location } = props;
 
-  const userState = useSelector((state:RootStore) => state.user);
+  const {selectedAddress,hasBroker} = useSelector((state:RootStore) => state.user);
 
   useEffect(() => {
-    if(isBind === undefined || userState.selectedAddress){
-      return
+    if(!selectedAddress){
+      dispatch(UserModel.actions.loadWallet());
+      return;
     }
 
-    if (!isBind) {
+    if (!hasBroker) {
       const rootPath = location.pathname.split("/")[1];
 
-      const menu = routes.find((men) => men.path.toLocaleLowerCase()===rootPath.toLocaleLowerCase());
+      const menu = routes.find((men) => men.path.toLowerCase()===location.pathname.toLowerCase());
+
       if(!menu){
-        bindBroker({trader: userState.selectedAddress,brokerId: rootPath}).then(() => {
+        bindBroker({trader: selectedAddress,brokerId: rootPath}).then(() => {
           history.push("/trade");
         }).catch((e) => {
           console.error("bind broker error", e);
         });
-      }else{
+      }else if(location.pathname.toLowerCase() !== brokerBindPath){
         history.push(brokerBindPath);
       }
       return;
@@ -49,7 +48,7 @@ const Home: React.FC<HomeProps> = props => {
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location.pathname, isBind]);
+  }, [location, hasBroker, selectedAddress]);
 
   return (
     <>
