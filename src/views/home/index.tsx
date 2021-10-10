655635223553e61424a5f072324bdd5840318d3e
huangce.zb <huangce.zb@alibaba-inc.com>
2021-10-11 03:33:07 +0800

    fix title

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useEffect, useState} from "react";
 import { RouteProps } from "@/router/types";
-import { Switch, Route, Redirect } from "react-router-dom";
+import {Switch, Route, Redirect, useLocation} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {RootStore, UserModel} from "@/store/index";
 import Nav from "./nav";
@@ -9,10 +9,68 @@ import IntlPro from "@/locales/index";
 import "./index.less";
 import {bindBroker} from "@/api/broker";
 import Trade from "@/views/trade";
+import {ReactComponent} from "*.svg";
+import {useAsync} from "react-use";
 
 interface HomeProps extends RouteProps {}
 const brokerBindPath = "/broker/bind";
 const tradePath = "/trade";
+
+const  RouteGuard: React.FC<HomeProps> = props => {
+  const {routes} = props;
+  const location = useLocation();
+  const {selectedAddress,hasBroker,slefBrokerId} = useSelector((state:RootStore) => state.user);
+  const {pathname} = location;
+
+  let routeConfig = routes.find(
+    (item) => {
+      return item.path.replace(/\s*/g,"") === pathname
+    }
+  );
+
+  if(!routeConfig) {
+    routeConfig = routes[0];
+  }
+
+  let targetRoute = null;
+  let redirectPath = null;
+
+  useEffect(() => {
+    if(hasBroker === undefined){
+      targetRoute = <></>
+    }else{
+
+      const currentRoute = <Trade/>;
+
+      const rootPath = location.pathname.split("/")[1];
+
+      if(rootPath === slefBrokerId){
+        targetRoute = currentRoute;
+        return;
+      }
+      const menu = routes.find((men) => men.path.toLowerCase()===(`/${rootPath}`).toLowerCase());
+
+      if (!hasBroker) {
+        if(!menu){
+          bindBroker({trader: selectedAddress,brokerId: rootPath}).then(() => {
+            redirectPath = tradePath;
+          }).catch((e) => {
+            console.error("bind broker error", e);
+          });
+        }else if(location.pathname.toLowerCase() !== brokerBindPath){
+          redirectPath = brokerBindPath;
+        }
+        return;
+      }
+
+      if(hasBroker && routeConfig){
+        targetRoute = currentRoute;
+      }
+    }
+  }, [location, hasBroker, selectedAddress]);
+
+  return <>{redirectPath ? <Redirect to={redirectPath}/> : targetRoute}</>
+}
 
 const Home: React.FC<HomeProps> = props => {
   const dispatch = useDispatch();
@@ -39,8 +97,6 @@ const Home: React.FC<HomeProps> = props => {
     const menu = routes.find((men) => men.path.toLowerCase()===(`/${rootPath}`).toLowerCase());
 
     if (!hasBroker) {
-
-
       if(!menu){
         bindBroker({trader: selectedAddress,brokerId: rootPath}).then(() => {
           doRedirect(tradePath);
@@ -68,17 +124,7 @@ const Home: React.FC<HomeProps> = props => {
         <Nav></Nav>
         <div className="main">
           <Switch>
-            {routes.map((route, i) => (
-              <Route
-                path={route.path}
-                exact={route.exact}
-                key={i}
-                render={props => (
-                  <route.component {...props} routes={route.routes} />
-                )}
-              />
-            ))}
-            {redirectPath  ? <Redirect from="/" to={`${redirectPath}`} /> : <Trade/>}
+            <RouteGuard {...props}/>
           </Switch>
         </div>
       </IntlPro>
