c957f9deead5a53fe55791a33b094899523e725a
huangce.zb <huangce.zb@alibaba-inc.com>
2021-10-16 01:47:38 +0800

    set build env

--- src/store/modules/data.ts
+++ src/store/modules/data.ts
@@ -23,7 +23,7 @@ const actions = {
       const history = await getHistoryTradingData(token)
 
       if(history && history.length > 0) {
-        current = history[0]
+        current = history[history.length-1]
       }
 
       return {current,history}
