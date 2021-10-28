52586bffa6fc2e9bd6d4b094629da4b00adf3c95
huangce.zb <huangce.zb@alibaba-inc.com>
2021-10-29 00:23:06 +0800

    update i18n text

--- src/utils/contractUtil.js
+++ src/utils/contractUtil.js
@@ -181,7 +181,7 @@ export const Token = {
 }
 
 
-const cache = {gasPrice: 1.5*1e9, gas: 21000, gasLimit: 21009}
+const cache = {gasPrice: 1.5*1e9,gas: 21000, gasLimit: 50000}
 let lastCacheTime = 0;
 export const contractDecimals = 8
 
