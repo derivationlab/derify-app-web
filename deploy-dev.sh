de03fdb5c52c5a323f04fde941625b816c88fdff
huangce.zb <huangce.zb@alibaba-inc.com>
2021-11-14 13:24:22 +0800

    update deploy script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,3 +3,4 @@ export REACT_APP_NODE_ENV='development'
 #git pull
 npm run build
 scp -i ~/Documents/test-new.pem -r ./build/* ubuntu@ec2-13-125-43-43.ap-northeast-2.compute.amazonaws.com:/var/www/html/web-test/
+echo " deploy finished at `date`";
