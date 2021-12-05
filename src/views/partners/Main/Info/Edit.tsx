3e11dc73321fc808f0f8e7f13e742834bf605526
huangce.zb <huangce.zb@alibaba-inc.com>
2021-12-05 16:24:55 +0800

    fix deposit/withdraw switch

--- src/views/partners/Main/Info/Edit.tsx
+++ src/views/partners/Main/Info/Edit.tsx
@@ -86,6 +86,11 @@ const Edit: React.FC<EditProps> = props => {
       return false
     }
 
+    if(!broker.introduction) {
+      setErrorMsg($t('Broker.Broker.InfoEdit.InfoRequired'))
+      return false
+    }
+
 
     if(!logoFile && !broker.logo) {
       setErrorMsg($t('Broker.Broker.InfoEdit.InfoRequired'))
